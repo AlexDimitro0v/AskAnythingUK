02da65229a802fc76472eb683156e53b7cc97298
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2019-11-07 18:38:40 +0000

    Logo Inserted

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login-page"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name="logout-page"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout-page"),
     path('register/', views.register, name="registration-page"),
 ]
