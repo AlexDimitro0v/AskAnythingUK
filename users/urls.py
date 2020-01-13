8f30011963e162079e2c81db8619531b59b82210
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2020-01-13 21:06:09 +0000

    Hiding Sensitive Information

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name="registration-page"),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login-page"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name="login-page"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout-page"),
     path('profile-page/', views.view_profile, name="profile-page"),
     path('customize-profile/', views.customize_user_profile, name="customize-profile-page"),
