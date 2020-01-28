88e8cbd0e07c8811aa584f31cf8890884397df89
Štěpán Brychta <stepan.brychta@me.com>
2020-01-28 22:27:23 +0000

    New sorting of requests based on priority & other minor changes

--- AskAnything/settings.py
+++ AskAnything/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 LOGIN_REDIRECT_URL = 'home-page'  # By default when logged in Django looks for a route in accounts/profile
 LOGIN_URL = 'login-page'          # Useful when login_required decorator is used; by default Django looks for a route in accounts/login
-# LOGOUT_REDIRECT_URL = 'logout-page'
+LOGOUT_REDIRECT_URL = 'landing-page'
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
