bab7955cf1eed052cb98580ab39c96a4c8ba839e
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2020-02-08 09:54:59 +0000

    Image Crop in Public Info form

--- users/forms.py
+++ users/forms.py
@@ -113,7 +113,29 @@ class PrivateInformationForm(forms.ModelForm):
 
 
 class PublicInformationForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput(), required=False)
+    y = forms.FloatField(widget=forms.HiddenInput(), required=False)
+    width = forms.FloatField(widget=forms.HiddenInput(), required=False)
+    height = forms.FloatField(widget=forms.HiddenInput(), required=False)
+    image = forms.ImageField(label='Profile Image', required=False, error_messages={'invalid': "Image files only"}, widget=forms.FileInput)
 
     class Meta:
         model = UserProfile
-        fields = ['city', 'description', 'linkedin', 'url_link_1', 'url_link_2']
+        fields = ['city', 'description', 'linkedin', 'url_link_1', 'url_link_2', 'image', 'x', 'y', 'width', 'height']
+
+    def save(self):
+        photo = super(PublicInformationForm, self).save()
+        if (self.cleaned_data.get('x') and self.cleaned_data.get('y') and self.cleaned_data.get('width')
+                and self.cleaned_data.get('height')):
+
+            x = self.cleaned_data.get('x')
+            y = self.cleaned_data.get('y')
+            w = self.cleaned_data.get('width')
+            h = self.cleaned_data.get('height')
+
+            image = Image.open(photo.image)
+            cropped_image = image.crop((x, y, w + x, h + y))
+            resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+            resized_image.save(photo.image.path)
+
+            return photo
