82f915168a9cd421393f5ca94e4b6d066486d7f1
Štěpán Brychta <stepan.brychta@me.com>
2020-02-15 12:26:46 +0000

    Smart Recommendations algorithm fix

--- main/smart_recommendations.py
+++ main/smart_recommendations.py
@@ -34,22 +34,31 @@ def get_most_common(text):
 
 def get_recommended_feedbackers(words1,frequencies1):
     recommended_users = []
+
+    # Not enough words to provide accurate recommendations
+    if len(words1) < 10: return recommended_users
+
+    # Use cosine similarity algorithm for each user
     for user in User.objects.all():
         words2 = user.userprofile.most_common_words
         frequencies2 = user.userprofile.most_common_words_numbers
         if not words2: continue
         if len(frequencies2) < len(words2): continue
+        if len(words2) < 10: continue
         total = 0
         w1_square = 0
         w2_square = 0
         for i in range(0,len(words1)):
             for j in range(0,len(words2)):
-                if words1[i][0] == words2[j][0]:
-                    total += frequencies1[i]*frequencies2[i]
+                if words1[i] == words2[j]:
+                    total += frequencies1[i]*frequencies2[j]
             w1_square += frequencies1[i]**(2)
             w2_square += frequencies2[i]**(2)
         total = total/(w1_square**(1/2) * w2_square**(1/2))
+
+        print(user,total)
+
+        # Recommend if similarity score higher than 0.3
         if total > 0.3:
-            print(user)
             recommended_users.append(user)
     return recommended_users
