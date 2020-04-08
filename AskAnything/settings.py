fa12bc21376a6b912cac0bb3b5e00acc04264268
Alexandar Dimitrov <40325490+AlexDimitro0v@users.noreply.github.com>
2020-04-08 10:22:21 +0100

    Updated Static_Root

--- AskAnything/settings.py
+++ AskAnything/settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_user_agents.middleware.UserAgentMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'AskAnything.urls'
@@ -162,6 +163,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # In order to website can find the media when we try to view them from within the browser, adjust:
